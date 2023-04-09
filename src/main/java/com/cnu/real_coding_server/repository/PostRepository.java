19b0dfbf04a8d04e995720f77cfd727471f8fd7e
JeungSeok <109063576+JeungSeok@users.noreply.github.com>
2023-04-09 20:21:37 +0900

    201902751 정석호 Project 과제

--- src/main/java/com/cnu/real_coding_server/repository/PostRepository.java
+++ src/main/java/com/cnu/real_coding_server/repository/PostRepository.java
@@ -3,11 +3,13 @@ package com.cnu.real_coding_server.repository;
 import com.cnu.real_coding_server.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
-//    Post save(Post post);
-//    List<Post> findAll();
-//    Optional<Post> findById(Integer postId);
-//    void delete(Post post);
+    Post save(Post post);
+    List<Post> findAll();
+    Optional<Post> findById(Integer postId);
+    void delete(Post post);
 }
